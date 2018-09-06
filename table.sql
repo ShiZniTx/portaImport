4d9bd6f7b58118283a4f8e5dbba59ebcf56e325d
ShiZniTx <sshiznit@yahoo.com>
2018-09-06 14:03:44 +0300

    databaseReady

--- table.sql
+++ table.sql
@@ -1,34 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Database: `portaImport`
+-- Host: localhost:3306
+-- Generation Time: 06 Sep 2018 la 14:02
+-- Versiune server: 5.6.19-67.0
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `r32256vo_portaImport`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comenzi`
+-- Structura de tabel pentru tabelul `comenzi`
 --
 
-CREATE TABLE IF NOT EXISTS `comenzi` (
+CREATE TABLE `comenzi` (
   `id` int(11) NOT NULL,
-  `CreatedDate` varchar(250) NOT NULL,
-  `OrderName` varchar(250) NOT NULL,
-  `OrderNo` varchar(250) NOT NULL,
-  `CatalogDescription` varchar(250) NOT NULL,
-  `CatalogNumber` text NOT NULL,
-  `Configuration` varchar(10) NOT NULL,
-  `Ean` varchar(250) NOT NULL,
-  `LineNo` varchar(100) NOT NULL,
-  `OrderCurrency` varchar(250) NOT NULL,
-  `OrderedQuantity` varchar(100) NOT NULL,
-  `PlannedDate` varchar(100) NOT NULL,
-  `ShippedQuantity` varchar(100) NOT NULL,
-  `SupplierItemCode` varchar(100) NOT NULL,
-  `TotalDiscount` varchar(100) NOT NULL,
-  `TotalWeight` varchar(100) NOT NULL,
-  `UnitGrossPriceDiscount` varchar(100) NOT NULL,
-  `UnitNetPrice` varchar(100) NOT NULL,
-  `UnitNetPriceDiscount` varchar(100) NOT NULL,
-  `UnitOfWeight` varchar(100) NOT NULL
+  `CreatedDate` date NOT NULL DEFAULT '0000-00-00',
+  `OrderName` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `OrderNo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `CatalogDescription` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `CatalogNumber` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `Configuration` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `Ean` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `LineNo` int(3) NOT NULL,
+  `OrderCurrency` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `OrderedQuantity` int(4) NOT NULL,
+  `PlannedDate` date NOT NULL DEFAULT '0000-00-00',
+  `ShippedQuantity` int(4) NOT NULL,
+  `SupplierItemCode` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `TotalDiscount` int(3) NOT NULL,
+  `TotalWeight` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `UnitGrossPriceDiscount` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `UnitNetPrice` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `UnitNetPriceDiscount` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `UnitOfWeight` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -50,3 +70,8 @@ ALTER TABLE `comenzi`
 --
 ALTER TABLE `comenzi`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
